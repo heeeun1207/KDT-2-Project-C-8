37a0eadebd767e94d7c974dd33d2711062e6f51b
jungheeeun <jungheeeun272@gmail.com>
2023-04-25 13:04:00 +0900

    친환경 라이프 페이지 연결하기

--- app.js
+++ app.js
@@ -53,12 +53,22 @@ const server = http.createServer((req, res) => {
         res.end(data);
       }
     });
+  } else if (req.url === '/ecolifePrac.html' && req.method === 'GET') {
+    fs.readFile('sub/ecolifePrac.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end('Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end(data);
+      }
+    });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
     res.end('Not Found');
   }
 });
 
-server.listen(3050, () => {
-  console.log('Server3050');
+server.listen(3080, () => {
+  console.log('Server3000');
 });
