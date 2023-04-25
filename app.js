dab25b39b3a7c977d8edcebd3deb8ac8debe5b84
jungheeeun <jungheeeun272@gmail.com>
2023-04-25 11:09:18 +0900

    URL ->  로그인 페이지 이동

--- app.js
+++ app.js
@@ -1,23 +1,45 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+  // 요청 
+  if (req.url === '/' && req.method === 'GET') {
+    fs.readFile('index.html', (err, data) => {
       if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' }); 
+        res.writeHead(500, { 'Content-Type': 'text/html; charset=utf-8' });
         res.end('Error');
       } else {
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' }); 
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/profile.png' && req.method === 'GET') {
+    //로그인 이미지 눌러서 넘어가기
+    fs.readFile('profile.png', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end('Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/login.html' && req.method === 'GET') {
+    // login.html 파일 읽어서 응답 전송
+    fs.readFile('login.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end('Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
         res.end(data);
       }
     });
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' }); 
+    res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
     res.end('Not Found');
   }
 });
 server.listen(3000, () => {
-  console.log('서버실행중');
+  console.log('ServerOK!');
 });
