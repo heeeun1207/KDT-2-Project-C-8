028c029d4b586fc6efe3c0261985d1de480eae05
jungheeeun <jungheeeun272@gmail.com>
2023-04-27 15:41:36 +0900

    주석정리

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ const fs = require('fs');
 const path = require('path');
 
 const routes = {
-  '/': { file: 'index.html', type: 'text/html; charset=utf-8' },
-  '/profile.png': { file: 'sub/profile.png', type: 'image/png' },
+  '/': { file: 'index.html', type: 'text/html; charset=utf-8' }, 
+  //프로필 이미지  // error
+  '/profile.png': { file: 'sub/profile.png', type: 'image/png' }, 
+  //로그인 페이지  
   '/login.html': { file: 'sub/login.html', type: 'text/html; charset=utf-8' },
+  //테스트 안내 페이지 
   '/testGuide.html': { file: 'sub/testpaper/testGuide.html', type: 'text/html; charset=utf-8' },
+  //이벤트페이지 (마크업 아직 안함 )
   '/eventprac.html': { file: 'sub/eventprac.html', type: 'text/html; charset=utf-8' },
+  //에코라이프 페이지
   '/ecoLife.html': { file: 'sub/ecoLifePage/ecoLife.html', type: 'text/html; charset=utf-8' },
+  //에코라이프 JS
   '/ecoscript.js': { file: 'sub/ecoLifePage/ecoscript.js', type: 'text/javascript; charset=utf-8' },
+  //에코라이프 CSS
   '/ecostyle.css': { file: 'sub/ecoLifePage/ecostyle.css', type: 'text/css; charset=utf-8' },
+  //지킴이 신고 페이지
   '/tellerpage.html': { file: 'sub/tellerpage.html', type: 'text/html; charset=utf-8' },
+  //추천 따봉 이미지 
   '/thunbup.png': { file: 'sub/thumbup.png', type: 'image/png' },
+  //비추천 이미지 
   '/thunbdown.png': { file: 'sub/thumbdown.png', type: 'image/png' },
 };
 
