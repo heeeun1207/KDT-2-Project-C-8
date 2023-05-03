abfca7abbefb1e11e01b1ff38e9ef9c3d472ed3b
jungheeeun <jungheeeun272@gmail.com>
2023-05-03 22:30:31 +0900

    webpack 안되는거 다시 봐보기

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,35 @@
-import fs from 'fs';
-export default {
-  enrty : "./src/index.html",
-  output : {
-    path : './dist',
-    //distrbute 준말로 보통 배포하는 파일을 모아두는 디레고리 구성이다 . 
-    //이렇게 path key를 사용해야 한다면 , 일반적으로 fs 모듈을 사용해야한다.
-  
-  filename : './index.bundle.js'
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
-   //* 파일명은 위와 같이 정적으로 만들어줄 수 있다.
-   //정의된 파일이긴 하지만 엄연히 모듈처리되는 파일이기 때문에 필요에 따라서 [name],[hash],[chunkhash]등 사용 가능.
+module.exports = {
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.html$/,
+  //       use: ['html-loader']
+  //     }
+  //   ]
+  // },
+  mode:'production', 
+  entry: '/src/index.html',
+  output: {
+    path: path.resolve(__dirname, 'dist'), // 상대경로를 절대경로로 변환
+    filename: './index.bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      filename:"test.html"
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: './src/assets',
+          to: './assets'
+        }
+      ]
+    })
+  ],
 }
