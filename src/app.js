6a3febc1beb77461bf13e715ee06315e4c4a2c3a
jungheeeun <jungheeeun272@gmail.com>
2023-05-04 09:46:18 +0900

    tellerpage text/html -> text/css

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ const routes = {
   '/ecoscript.js': { file: 'assets/js/ecoscript.js', type: 'text/javascript; charset=utf-8' },
   //신고하는 지킴이페이지 
   '/tellerpage.html': { file: 'assets/css/tellerpage/tellerpage.html', type: 'text/html; charset=utf-8' },
-  '/tellerpage.css': { file: 'assets/css/tellerpage/tellerpage.css', type: 'text/html; charset=utf-8' },
+  '/tellerpage.css': { file: 'assets/css/tellerpage/tellerpage.css', type: 'text/css; charset=utf-8' },
   '/tellerpage.js': { file: 'assets/js/tellerpage.js', type: 'text/javascript; charset=utf-8' },
   '/thumbup.png': { file: 'assets/images/thumbup.png', type: 'image/png' },
   '/thumbdown.png': { file: 'assets/images/thumbdown.png', type: 'image/png' },
