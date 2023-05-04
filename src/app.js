fdb22664c2d2a3453ede29845bb01df204df3779
jungheeeun <jungheeeun272@gmail.com>
2023-05-04 17:26:11 +0900

    views 폴더에 -> html 파일 넣기

--- src/app.js
+++ src/app.js
@@ -19,8 +19,6 @@ connection.connect((error) => {
 
 const routes = {
   '/': { file: '/index.html', type: 'text/html; charset=utf-8' },
-  // '/': { file: 'assets/css/index/index.css', type: 'text/css; charset=utf-8' }, 
-  // '/': { file: 'assets/js/index.js', type: 'text/javascript' }, 
   '/profile.png': { file: 'assets/images/profile.png', type: 'image/png' },
   '/homegreen.png': { file: 'assets/images/homegreen.png', type: 'image/png' },
   '/home.png': { file: 'assets/images/home.png', type: 'image/png' },
@@ -40,39 +38,39 @@ const routes = {
   '/test.png': { file: 'assets/images/main/test.png', type: 'image/png' },
   '/user.png': { file: 'assets/images/main/user.png', type: 'image/png' },
   //로그인 화면 
-  '/login.html': { file: 'assets/css/Login/login.html', type: 'text/html; charset=utf-8' },
+  '/login.html': { file: 'views/login.html', type: 'text/html; charset=utf-8' },
   '/Login.css': { file: 'assets/css/Login/Login.css', type: 'text/css; charset=utf-8' },
   //마이페이지 
-  '/mypage.html': { file: 'assets/css/mypage/mypage.html', type: 'text/html; charset=utf-8' },
+  '/mypage.html': { file: 'views/mypage.html', type: 'text/html; charset=utf-8' },
   '/mypage.css': { file: 'assets/css/mypage/mypage.css', type: 'text/css; charset=utf-8' },
   //지킴이 테스트 첫화면 (안내 페이지)
-  '/testGuide.html': { file: 'assets/css/testpaper/testGuide.html', type: 'text/html; charset=utf-8' },
+  '/testGuide.html': { file: 'views/testGuide.html', type: 'text/html; charset=utf-8' },
   '/testGuide.css': { file: 'assets/css/testpaper/testGuide.css', type: 'text/css; charset=utf-8' },
   '/testGuide.js': { file: 'assets/js/testGuide.js', type: 'text/javascript ; charset=utf-8' },
   //테스트 시험지 페이지 
-  '/test.html': { file: 'assets/css/testpaper/test.html', type: 'text/html; charset=utf-8' },
+  '/test.html': { file: 'views/test.html', type: 'text/html; charset=utf-8' },
   '/test.css': { file: 'assets/css/testpaper/test.css', type: 'text/css; charset=utf-8' },
   '/test.js': { file: 'assets/js/test.js', type: 'text/javascript; charset=utf-8' },
   '/x-symbol.png': { file: 'assets/images/x-symbol.png', type: 'image/png' },
   //이벤트 & 소식페이지 
-  '/eventpage.html': { file: 'assets/css/News/eventpage.html', type: 'text/html; charset=utf-8' },
+  '/eventpage.html': { file: 'views/eventpage.html', type: 'text/html; charset=utf-8' },
   '/eventpage.css': { file: 'assets/css/News/eventpage.css', type: 'text/css; charset=utf-8' },
   //친환경 라이프페이지 
-  '/ecoLife.html': { file: 'assets/css/ecoLifePage/ecoLife.html', type: 'text/html; charset=utf-8' },
+  '/ecoLife.html': { file: 'views/ecoLife.html', type: 'text/html; charset=utf-8' },
   '/ecostyle.css': { file: 'assets/css/ecoLifePage/ecostyle.css', type: 'text/css; charset=utf-8' },
   '/ecoscript.js': { file: 'assets/js/ecoscript.js', type: 'text/javascript; charset=utf-8' },
   //신고하는 지킴이페이지 
-  '/tellerpage.html': { file: 'assets/css/tellerpage/tellerpage.html', type: 'text/html; charset=utf-8' },
+  '/tellerpage.html': { file: 'views/tellerpage.html', type: 'text/html; charset=utf-8' },
   '/tellerpage.css': { file: 'assets/css/tellerpage/tellerpage.css', type: 'text/css; charset=utf-8' },
   '/tellerpage.js': { file: 'assets/js/tellerpage.js', type: 'text/javascript; charset=utf-8' },
   '/thumbup.png': { file: 'assets/images/thumbup.png', type: 'image/png' },
   '/thumbdown.png': { file: 'assets/images/thumbdown.png', type: 'image/png' },
   //FAQ 페이지 
-  '/FAQpage.html': { file: 'assets/css/FAQ/FAQpage.html', type: 'text/html; charset=utf-8' },
+  '/FAQpage.html': { file: 'views/FAQpage.html', type: 'text/html; charset=utf-8' },
   '/FAQpage.css': { file: 'assets/css/FAQ/FAQpage.css', type: 'text/css; charset=utf-8' },
   '/FAQpage.js': { file: 'assets/js/FAQpage.js', type: 'text/jcssavascript; charset=utf-8' },
   //공지사항 페이지 
-  '/NoticePage.html': { file: 'assets/css/Notice/NoticePage.html', type: 'text/html; charset=utf-8' },
+  '/NoticePage.html': { file: 'views/NoticePage.html', type: 'text/html; charset=utf-8' },
   '/NoticePage.css': { file: 'assets/css/Notice/NoticePage.css', type: 'text/css; charset=utf-8' },
 };
 
@@ -176,5 +174,5 @@ handleResponse(request, response);
 });
 
 server.listen(3000, () => {
-console.log('Server started on port 3000.');
+console.log('Server 3000');
 });
