61dafaa8b6f919465a533a9c2e0d5c3ad07691b8
jungheeeun <jungheeeun272@gmail.com>
2023-05-05 18:15:16 +0900

    express... nodemon....

---
+++ testDB/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const port = 4000;
+app.listen(port, () => {
+  console.log("Server listening on port", port)
+});
