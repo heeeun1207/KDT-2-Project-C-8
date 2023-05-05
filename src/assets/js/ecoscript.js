674e91cc4f4bd283aa188ad722c3c245c1f0e0a3
jungheeeun <jungheeeun272@gmail.com>
2023-05-05 10:55:19 +0900

    친환경 라이프 모달 수정중

--- src/assets/js/ecoscript.js
+++ src/assets/js/ecoscript.js
@@ -1,3 +1,42 @@
-document.getElementById('profile-image').addEventListener('click', function() {
-  console.log('Profile img click');
-});
+
+  // 모달 요소 가져오기
+  const modal = document.getElementById("modal");
+
+  // 각 섹션 요소 가져오기
+  const section1 = document.getElementById("section1");
+  const section2 = document.getElementById("section2");
+  const section3 = document.getElementById("section3");
+  const section4 = document.getElementById("section4");
+  const section5 = document.getElementById("section5");
+  const section6 = document.getElementById("section6");
+
+  // 각 섹션 요소를 클릭하면 모달을 띄우는 함수
+  section1.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
+  section2.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
+  section3.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
+  section4.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
+  section5.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
+  section6.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
+
+  // 모달 외부를 클릭하면 모달을 닫는 함수
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+
+
+    
