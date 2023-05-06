964f3caa5baaa9b4de44903926e8a34313aad801
jungheeeun <jungheeeun272@gmail.com>
2023-05-06 19:24:53 +0900

    각 section에  에코라이프 새로운 창 만들기

--- src/assets/js/ecoscript.js
+++ src/assets/js/ecoscript.js
@@ -1,42 +1,31 @@
-
-  // 모달 요소 가져오기
-  const modal = document.getElementById("modal");
-
-  // 각 섹션 요소 가져오기
-  const section1 = document.getElementById("section1");
-  const section2 = document.getElementById("section2");
-  const section3 = document.getElementById("section3");
-  const section4 = document.getElementById("section4");
-  const section5 = document.getElementById("section5");
-  const section6 = document.getElementById("section6");
-
-  // 각 섹션 요소를 클릭하면 모달을 띄우는 함수
-  section1.addEventListener("click", () => {
-    modal.style.display = "block";
-  });
-  section2.addEventListener("click", () => {
-    modal.style.display = "block";
-  });
-  section3.addEventListener("click", () => {
-    modal.style.display = "block";
-  });
-  section4.addEventListener("click", () => {
-    modal.style.display = "block";
-  });
-  section5.addEventListener("click", () => {
-    modal.style.display = "block";
-  });
-  section6.addEventListener("click", () => {
-    modal.style.display = "block";
-  });
-
-  // 모달 외부를 클릭하면 모달을 닫는 함수
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+function createNewPage(sectionNumber) {
+  var pageTitle, pageContent;
+  if (sectionNumber == 1) {
+    pageTitle = "Section 1 Page";
+    pageContent = "This is section 1.";
+  } else if (sectionNumber == 2) {
+    pageTitle = "Section 2 Page";
+    pageContent = "This is section 2.";
+  } else if (sectionNumber == 3) {
+    pageTitle = "Section 3 Page";
+    pageContent = "This is section 3.";
+  } else if (sectionNumber == 4) {
+    pageTitle = "Section 3 Page";
+    pageContent = "This is section 4.";
+  } else if (sectionNumber == 5) {
+    pageTitle = "Section 3 Page";
+    pageContent = "This is section 5.";
+  } else if (sectionNumber == 6) {
+    pageTitle = "Section 3 Page";
+    pageContent = "This is section 6.";
+  } else if (sectionNumber == 7) {
+    pageTitle = "Section 3 Page";
+    pageContent = "This is section 7.";
+  } else if (sectionNumber == 8) {
+    pageTitle = "Section 3 Page";
+    pageContent = "This is section 8.";
   }
 
-
-
-    
+  var newPage = window.open('newpage.html', '_blank', 'width=390,height=844');
+  newPage.document.write('<html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>' + pageTitle + '</title></head><body>' + pageContent + '</body></html>');
+}
