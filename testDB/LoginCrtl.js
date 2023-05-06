cd0f604fc5510fcdc7cc860feedc712dc1411142
jungheeeun <jungheeeun272@gmail.com>
2023-05-06 15:23:24 +0900

    왜 접근 거부일까 ? mysql 들어가지는데

--- testDB/LoginCrtl.js
+++ testDB/LoginCrtl.js
@@ -2,8 +2,9 @@ const connection = require('./db.config')
 
 const LoginCtrl ={
   getLogin : async (req,res) => {
-    connection.query('SELECT * FROM loginTest.Login', (error, rows) => {
+    connection.query('SELECT id FROM logintest2.logintest2', (error, rows) => {
       if(error) throw error;
+      console.log(rows);
       res.send(rows);
       
     })
