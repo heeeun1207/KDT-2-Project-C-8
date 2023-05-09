21a3ff0f3ee027a7835922a770ba3823e5d81862
jungheeeun <jungheeeun272@gmail.com>
2023-05-09 11:55:21 +0900

    신고페이지 지역선택 콤보박스 추가 -> 스타일 수정 필요함

--- src/assets/js/tellerpage.js
+++ src/assets/js/tellerpage.js
@@ -1,3 +1,4 @@
+const sidoElement = document.getElementById('sido');
 let counter = 0;
 let clicked = false;
 function incrementCounter() {
@@ -41,3 +42,38 @@ imageInput.addEventListener('change', () => {
   };
   reader.readAsDataURL(file);
 });
+
+
+
+const gugunList = {
+  '서울특별시': [],
+  '경기도': [],
+  '인천광역시': [],
+  '강원도': [],
+  '충청북도': [],
+  '충청남도': [],
+  '대전광역시': [],
+  '경상북도': [],
+  '경상남도': [],
+  '전라북도': [],
+  '전라남도': [],
+  '대구광역시': [],
+  '부산광역시': [],
+  '울산광역시': [],
+  '광주광역시': [],
+  '제주특별자치도': []
+};
+
+function changeSido() {
+  const selectedSido = sidoElement.value;
+  const gugunElement = document.getElementById('gugun');
+  gugunElement.innerHTML = '<option value="">시,도를 선택하세요</option>';
+  if (selectedSido) {
+    const guguns = gugunList[selectedSido];
+    for (let i = 0; i < guguns.length; i++) {
+      const gugun = guguns[i];
+      gugunElement.innerHTML += `<option value="${gugun}">${gugun}</option>`;
+
+// 최종적으로 구/군 콤보박스를 화면에 표시
+document.querySelector('#gugun').appendChild(gugunElement);
+    }}};
