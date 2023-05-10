17b87a9a9477edc16ba6cf19064ae04ae08973e8
jungheeeun <jungheeeun272@gmail.com>
2023-05-10 15:11:57 +0900

    신고페이지 수정중

--- src/assets/js/tellerpage.js
+++ src/assets/js/tellerpage.js
@@ -59,33 +59,11 @@ function changeSido() {
   if (sidoSelect.value) {
     gunguSelect.disabled = false;
 
-    // 선택된 시/도에 해당하는 군/구 정보를 가져와서 콤보박스에 추가
-    // 예시 데이터: {"서울특별시": ["강남구", "서초구", ...], "경기도": ["수원시", "용인시", ...], ...}
+ 
     var gunguList = getGunguList(sidoSelect.value);
     gunguList.forEach(function (gungu) {
       gunguSelect.innerHTML += "<option value='" + gungu + "'>" + gungu + "</option>";
     });
   }
 }
-
-function changeGungu() {
-  var sidoSelect = document.getElementById("sido");
-  var gunguSelect = document.getElementById("gungu");
-  var dongSelect = document.getElementById("dong");
-
-  dongSelect.innerHTML = "<option value=''>선택 안 함</option>";
-  dongSelect.disabled = true;
-
-  if (gunguSelect.value) {
-    dongSelect.disabled = false;
-
-    // 선택된 군/구에 해당하는 동 정보를 가져와서 콤보박스에 추가
-    // 예시 데이터: {"강남구": ["개포동", "논현동", ...], "서초구": ["반포동", "양재동", ...], ...}
-    var dongList = getDongList(sidoSelect.value, gunguSelect.value);
-    dongList.forEach(function (dong) {
-      dongSelect.innerHTML += "<option value='" + dong + "'>" + dong + "</option>";
-    });
-  }
-}
-
 
