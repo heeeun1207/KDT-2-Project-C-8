9239b03a4cdec5252e9348724c09d1c49211ab80
jungheeeun <jungheeeun272@gmail.com>
2023-05-10 15:44:02 +0900

    회원가입 누르면 마이페이지 가기 -> X

--- src/assets/js/register.js
+++ src/assets/js/register.js
@@ -1,7 +1,11 @@
-function register() {
+function register(event) {
+  event.preventDefault(); // 기본 동작 막기
+  window.location.href = "Submypage.png"; // 페이지 이동
+
   var id = document.getElementById("id").value;
   var name = document.getElementById("name").value;
   var password = document.getElementById("password").value;
+  
 
   // 입력값 유효성 검사
   if (id === "" || name === "" || password === "") {
@@ -9,6 +13,8 @@ function register() {
     return;
   }
 
-  //코드 작성 필요 
+  // 회원가입 완료 메시지와 함께 페이지 이동
   alert("회원가입이 완료되었습니다.");
+  window.location.href = "Submypage.html";
 }
+
