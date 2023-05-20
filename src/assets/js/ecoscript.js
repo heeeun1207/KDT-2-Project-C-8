b5391232c53a8b4abe3bdae83a4feb9df55fabd0
jungheeeun <jungheeeun272@gmail.com>
2023-05-20 19:51:56 +0900

    무라벨이 왜 환경보호에 좋은지 설명하기

--- src/assets/js/ecoscript.js
+++ src/assets/js/ecoscript.js
@@ -66,26 +66,28 @@ document.addEventListener('DOMContentLoaded', function() {
         case 'section3':
           modalText = `
           <p style="color: black; font-size: 50px;">
-          텀블러 사용하기
+          PET 라벨 떼기 
           </p>
           <br>
           <img src="nolabel.jpeg" style="width: 800px; height: 500px;">
           <p style="color: black; font-size: 50px;">
-          다 쓴 일회용 컵은 어디로 갈까요?
+          페트병 라벨이 뭐길래?
           </p>
           <br>
           <p style="color: black; font-size: 30px;">
-          소각하거나 매립 시 심각한 토양 오염의 요인이 돼요.
+          재활용하기 위해서는 병에 붙은 라벨을 제거하는 과정이 필수에요. 
           </p>
           <br>
           <p style="color: black; font-size: 48px;">
-          그렇다면 재활용률은 얼마나 될까요?
+          페트병의 라벨을 뗀 후 분리배출하도록
+          <br>
+          정책이 변경되었어요!
           </p>
           <br>
           <p style="color: black; font-size: 30px;">
-          일회용 플라스틱 컵의 재활용률은 약 5%, 
+          일반 플라스틱류를 더 고품질로 재활용할 수 있기 때문이에요.
           <br>
-          일회용 종이 컵의 재활용률은 약 1%에 불과해요.
+          페트병 라벨이 없는 무라벨 생수를 사용해주세요!
           </p>`;
           break;
 
