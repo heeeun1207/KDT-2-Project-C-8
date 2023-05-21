ba332208fd5a2864d7387d2c331bfbe22597d21f
jungheeeun <jungheeeun272@gmail.com>
2023-05-21 13:17:13 +0900

    clicked 변수 추가하기

--- src/assets/js/tellerpage.js
+++ src/assets/js/tellerpage.js
@@ -1,46 +1,60 @@
+
+// ‘추천 표시’ 유효성 검사 처리 (한번만 기능하도록 제어)
 let counter = 0;
 let clicked = false;
+
 function incrementCounter() {
-  if (!clicked) { 
-    counter++; 
-    document.getElementById("counter").innerText = counter; 
-  }};
-  const imageInput = document.querySelector('#image-input');
+  if (!clicked) {
+    counter++;
+    document.getElementById("counter").innerText = counter;
+    clicked = true;
+  }
+}
+
+const imageInput = document.querySelector('#image-input');
 const imagePreview = document.querySelector('#image-preview');
 
 imageInput.addEventListener('change', () => {
-  const file = imageInput.files[0];
-  const reader = new FileReader();
+  if (!clicked) {
+    const file = imageInput.files[0];
+    const reader = new FileReader();
 
-  reader.onload = () => {
-    const image = new Image();
-    image.src = reader.result;
+    reader.onload = () => {
+      const image = new Image();
+      image.src = reader.result;
 
-    image.onload = () => {
-      const imageWidth = image.width;
-      const imageHeight = image.height;
-      const aspectRatio = imageWidth / imageHeight;
-      const maxWidth = window.innerWidth * 0.4;
-      const maxHeight = window.innerHeight * 0.4;
-      let width = maxWidth;
-      let height = maxHeight / aspectRatio;
+      image.onload = () => {
+        const imageWidth = image.width;
+        const imageHeight = image.height;
+        const aspectRatio = imageWidth / imageHeight;
+        const maxWidth = window.innerWidth * 0.4;
+        const maxHeight = window.innerHeight * 0.4;
+        let width = maxWidth;
+        let height = maxHeight / aspectRatio;
 
-      if (height > maxHeight) {
-        height = maxHeight;
-        width = height * aspectRatio;
-      }
-      imagePreview.innerHTML = '';
-      imagePreview.appendChild(image);
-      // image.style.width = `${width}px`;
-      // image.style.height = `${height}px`;
-      image.style.width = `250px`;
-      image.style.height = `260px`;
-      image.style.display = 'block';
-      image.style.margin = 'auto';
+        if (height > maxHeight) {
+          height = maxHeight;
+          width = height * aspectRatio;
+        }
+        imagePreview.innerHTML = '';
+        imagePreview.appendChild(image);
+        image.style.width = `250px`;
+        image.style.height = `260px`;
+        image.style.display = 'block';
+        image.style.margin = 'auto';
+        clicked = true;
+      };
     };
-  };
-  reader.readAsDataURL(file);
+    reader.readAsDataURL(file);
+  }
 });
+
+
+
+
+
+
+// 동네 선택하기 
 function changeSido() {
   var sidoSelect = document.getElementById("sido");
   var gunguSelect = document.getElementById("gungu");
