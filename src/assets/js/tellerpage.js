d5db34bd2b92af39d2a9a11e6d1b14cc5e3bbd73
jungheeeun <jungheeeun272@gmail.com>
2023-05-26 00:37:58 +0900

    슬라이드 하는중

--- src/assets/js/tellerpage.js
+++ src/assets/js/tellerpage.js
@@ -1,10 +1,33 @@
-
-// ‘좋아요 표시’ 한번만 기능하도록 제어하기. 
-// 싫어요 기능 추가하고 한번만 기능하도록 제어하기 .
 let likeCounter = 0;
 let dislikeCounter = 0;
 let likeClicked = false;
 let dislikeClicked = false;
+
+const imageInput = document.querySelector('#image-input');
+const imagePreview = document.querySelector('#image-preview');
+
+let slideIndex = 1;
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("slide");
+  
+  if (n > slides.length) {
+    slideIndex = 1;
+  } else if (n < 1) {
+    slideIndex = slides.length;
+  }
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex - 1].style.display = "block";
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
 
 function incrementCounter() {
   if (!likeClicked) {
@@ -21,9 +44,6 @@ function incrementCounterMinus() {
     dislikeClicked = true;
   }
 }
-
-const imageInput = document.querySelector('#image-input');
-const imagePreview = document.querySelector('#image-preview');
 
 imageInput.addEventListener('change', () => {
   if (!likeClicked && !dislikeClicked) {
@@ -47,23 +67,64 @@ imageInput.addEventListener('change', () => {
           height = maxHeight;
           width = height * aspectRatio;
         }
-        imagePreview.innerHTML = '';
-        imagePreview.appendChild(image);
-        // image.style.width = `${width}px`;
-        // image.style.height = `${height}px`;
-        image.style.width = `300px`;
-        image.style.height = `300px`;
-        image.style.display = 'block';
-        image.style.margin = 'auto';
-        image.style.borderRadius = '30px';
-        likeClicked = true;
-        dislikeClicked = true;
 
-        //다운로드 
+        
+        const slideElement = document.createElement('div');
+        slideElement.classList.add('slide');
+        slideElement.appendChild(image);
+        imagePreview.innerHTML = ''; // Clear previous slides
+        imagePreview.appendChild(slideElement);
+
+        //다음버튼 
+        const prevButton = document.createElement('button');
+        prevButton.textContent = '이전';
+        prevButton.addEventListener('click', () => plusSlides(-1));
+        const nextButton = document.createElement('button');
+        nextButton.textContent = '다음';
+        nextButton.addEventListener('click', () => plusSlides(1));
+
+        
+        imagePreview.appendChild(prevButton);
+        imagePreview.appendChild(nextButton);
+
+        showSlides(slideIndex);
+
+        // 버튼 , 이미지 
+        const container = document.createElement('div');
+        container.classList.add('image-container');
+        container.style.maxWidth = `${maxWidth}px`;
+        container.style.margin = 'auto';
+
+        // 버튼 
+        const likeButton = document.createElement('button');
+        likeButton.textContent = '👍';
+        likeButton.addEventListener('click', incrementCounter);
+        const dislikeButton = document.createElement('button');
+        dislikeButton.textContent = '👎';
+        dislikeButton.addEventListener('click', incrementCounterMinus);
+
+        // 추천 비추천
+        const likeCounterElement = document.createElement('span');
+        likeCounterElement.id = 'like-counter';
+        likeCounterElement.textContent = likeCounter;
+        const dislikeCounterElement = document.createElement('span');
+        dislikeCounterElement.id = 'dislike-counter';
+        dislikeCounterElement.textContent = dislikeCounter;
+
+        
+        container.appendChild(likeButton);
+        container.appendChild(likeCounterElement);
+        container.appendChild(dislikeButton);
+        container.appendChild(dislikeCounterElement);
+
+        
+        imagePreview.appendChild(container);
+
+        // 다운로드하기
         const downloadButton = document.createElement('a');
         downloadButton.href = reader.result;
         downloadButton.download = 'image.png';
-        downloadButton.textContent = '"사진 다운로드하기"';
+        downloadButton.textContent = '사진 다운로드하기';
         downloadButton.classList.add('download-button');
         imagePreview.appendChild(downloadButton);
       };
@@ -73,57 +134,52 @@ imageInput.addEventListener('change', () => {
 });
 
 
+// // 동네 선택하기 
+// function changeSido() {
+//   var sidoSelect = document.getElementById("sido");
+//   var gunguSelect = document.getElementById("gungu");
+//   var dongSelect = document.getElementById("dong");
+
+//   gunguSelect.innerHTML = "<option value=''>선택 안 함</option>";
+//   dongSelect.innerHTML = "<option value=''>선택 안 함</option>";
+//   gunguSelect.disabled = true;
+//   dongSelect.disabled = true;
+
+//   if (sidoSelect.value) {
+//     gunguSelect.disabled = false;
+//     var gunguList = getGunguList(sidoSelect.value);
+//     gunguList.forEach(function (gungu) {
+//       gunguSelect.innerHTML += "<option value='" + gungu + "'>" + gungu + "</option>";
+//     });
+
+//     gunguSelect.addEventListener('change', function() {
+//       var dongList = getDongList(sidoSelect.value, gunguSelect.value);
+//       dongSelect.innerHTML = "<option value=''>선택 안 함</option>";
+//       if (dongList.length > 0) {
+//         dongSelect.disabled = false;
+//         dongList.forEach(function (dong) {
+//           dongSelect.innerHTML += "<option value='" + dong + "'>" + dong + "</option>";
+//         });
+//       }
+//     });
+//   }
+// }
 
 
-
-
-
-// 동네 선택하기 
-function changeSido() {
-  var sidoSelect = document.getElementById("sido");
-  var gunguSelect = document.getElementById("gungu");
-  var dongSelect = document.getElementById("dong");
-
-  gunguSelect.innerHTML = "<option value=''>선택 안 함</option>";
-  dongSelect.innerHTML = "<option value=''>선택 안 함</option>";
-  gunguSelect.disabled = true;
-  dongSelect.disabled = true;
-
-  if (sidoSelect.value) {
-    gunguSelect.disabled = false;
-    var gunguList = getGunguList(sidoSelect.value);
-    gunguList.forEach(function (gungu) {
-      gunguSelect.innerHTML += "<option value='" + gungu + "'>" + gungu + "</option>";
-    });
-
-    gunguSelect.addEventListener('change', function() {
-      var dongList = getDongList(sidoSelect.value, gunguSelect.value);
-      dongSelect.innerHTML = "<option value=''>선택 안 함</option>";
-      if (dongList.length > 0) {
-        dongSelect.disabled = false;
-        dongList.forEach(function (dong) {
-          dongSelect.innerHTML += "<option value='" + dong + "'>" + dong + "</option>";
-        });
-      }
-    });
-  }
-}
-
-
-  function getGunguList(sido) {
-    let gunguList = [];
+//   function getGunguList(sido) {
+//     let gunguList = [];
   
-    switch (sido) {
-      case '서울특별시':
-        gunguList = ['강남구', '강동구', '강북구', '강서구', '관악구', '광진구', '구로구', '금천구', '노원구', '도봉구', '동대문구', '동작구', '마포구', '서대문구', '서초구', '성동구', '성북구', '송파구', '양천구', '영등포구', '용산구', '은평구', '종로구', '중구', '중랑구'];
-        break;
-      case '대전광역시':
-        gunguList =['서구','중구','동구','대덕구']
-      break;
-        default:gunguList=`내용이없습니다`;
-}}
+//     switch (sido) {
+//       case '서울특별시':
+//         gunguList = ['강남구', '강동구', '강북구', '강서구', '관악구', '광진구', '구로구', '금천구', '노원구', '도봉구', '동대문구', '동작구', '마포구', '서대문구', '서초구', '성동구', '성북구', '송파구', '양천구', '영등포구', '용산구', '은평구', '종로구', '중구', '중랑구'];
+//         break;
+//       case '대전광역시':
+//         gunguList =['서구','중구','동구','대덕구']
+//       break;
+//         default:gunguList=`내용이없습니다`;
+// }}
 
-function getDongList(sido, gungu) {
-  // sido, gungu 값에 따른 dong 목록을 반환하는 코드
-}
+// function getDongList(sido, gungu) {
+//   // sido, gungu 값에 따른 dong 목록을 반환하는 코드
+// }
 
